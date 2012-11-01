bb8f73e90abfb8ff46bec36c3005dfaa34525c0a
Rodrigo Delduca <rodrigodelduca@gmail.com>
2012-11-01 20:36:11 -0200

    First blood!

--- models.py
+++ models.py
@@ -1,2 +1,10 @@
+# -*- coding: utf-8 -*-
 
+from google.appengine.ext import db
+
+
+class Menu(db.Model):
+    foods = db.StringListProperty()
+    date = db.DateProperty()
+    stars = db.IntegerProperty()
 
--- views.py
+++ views.py
@@ -1,8 +1,13 @@
-
+# -*- coding: utf-8 -*-
 import os
 import json
+import datetime
 import xlrd
+import json
 import webapp2
+from models import Menu
+from google.appengine.api import memcache
+from google.appengine.ext import db
 from google.appengine.api import memcache
 
 
@@ -12,6 +17,10 @@ class MainHandler(webapp2.RequestHandler):
         self.response.out.write("PositaGrill")
 
 class UploadHandler(webapp2.RequestHandler):
+    START_OFFSET = 3
+    ROW_OFFSET   = 12
+    SHEET_NAME = 'Sheet2'
+
     def get(self):
         self.response.write('<html><body>')
         self.response.write('<form method="post" action="/upload" enctype="multipart/form-data">')
@@ -24,18 +33,35 @@ class UploadHandler(webapp2.RequestHandler):
         upload = self.request.get('file')
 
         workbook = xlrd.open_workbook(file_contents=upload)
-        worksheet = workbook.sheet_by_name('Sheet2')
+        worksheet = workbook.sheet_by_name(self.SHEET_NAME)
+        rows = worksheet.nrows - 1
+        columns = worksheet.ncols - 1
+        current_column = 0
 
-        num_rows = worksheet.nrows - 1
-        current_row = -1
+        while current_column < columns - 1:
+            current_column += 1
+            current_row = self.START_OFFSET
+            while current_row < rows:
+                foods = []
+                date = None
 
-        while current_row < num_rows:
-            current_row += 1
-            row = worksheet.row(current_row)
-            self.response.write(row)
+                for row in xrange(current_row, current_row + self.ROW_OFFSET):
+                    cell_type = worksheet.cell_type(row, current_column)
+                    cell_value = worksheet.cell_value(row, current_column)
 
+                    if cell_type == 3: # datetime
+                        date = datetime.datetime(*xlrd.xldate_as_tuple(cell_value, workbook.datemode)).date()
+                    else:
+                        foods.append(cell_value)
+
+                menu = Menu()
+                menu.foods = foods
+                menu.date = date
+                menu.save()
+
+                current_row += self.ROW_OFFSET
 
 class MenuHandler(webapp2.RequestHandler):
     def get(self, day, month, year):
-        self.response.out.write(str(year))
+        self.response.out.write(json.dumps(Menu.all()))
 
